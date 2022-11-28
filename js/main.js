63c73b4856a4e91ae0b3f488463c7d419eb8d1ce
Codingwithchantal <c_buerger@gmx.net>
2022-11-28 11:06:02 +0100

    initialised a new project structure: html todolist body and javascript algorithm to copy value of text field and display in ul

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function addTodo() {
+// accessing the input field, cloning the content with the method value
+let inputField = document.querySelector(".TodoListField").value;
+console.log(inputField);
+
+if (inputField !== "") {
+
+// converting the cloned text from inputField into a textNode
+    let todosTextNode = document.createTextNode(inputField);
+    // storing the textNode in a li with the method createElement
+    let listItemTextNode = document.createElement("li");
+    listItemTextNode.appendChild(todosTextNode);
+    // append the listitemTextnode as a child to the ul
+    document.querySelector(".todoUL").appendChild(listItemTextNode);
+
+    // access input field and clear it
+    document.querySelector(".TodoListField").value = "";
+}
+}
+
